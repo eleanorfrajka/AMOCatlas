09c486ba8e4b5a429a1b2946de5a87b0a90f9d1b
Isabelle Schmitz <isabelle.sh@gmx.de>
2025-12-16 15:55:00 +0100

    [FIX] Download SAMBA data (#93)
    * [FIX] download SAMBA data
    
    Changed the  reader, so it correctly selectes the samba data from Meinen et al and Kersale et al.

--- amocatlas/read_samba.py
+++ amocatlas/read_samba.py
@@ -21,8 +21,8 @@ SAMBA_TRANSPORT_FILES = [
 ]
 # Mapping of filenames to remote URLs
 SAMBA_FILE_URLS = {
-    "Upper_Abyssal_Transport_Anomalies.txt": "ftp://ftp.aoml.noaa.gov/phod/pub/SAM/2020_Kersale_etal_ScienceAdvances/",
-    "MOC_TotalAnomaly_and_constituents.asc": "https://www.aoml.noaa.gov/phod/SAMOC_international/documents/",
+    "Upper_Abyssal_Transport_Anomalies.txt": "ftp://ftp.aoml.noaa.gov/phod/pub/SAM/2020_Kersale_etal_ScienceAdvances/Upper_Abyssal_Transport_Anomalies.txt",
+    "MOC_TotalAnomaly_and_constituents.asc": "https://www.aoml.noaa.gov/phod/SAMOC_international/documents/MOC_TotalAnomaly_and_constituents.asc",
 }
 
 # Global metadata for SAMBA
